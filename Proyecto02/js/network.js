74d77c207f25f995b1a96a42fa5ea29b45ea53a5
Michelle Mujica <mmujica@espol.edu.ec>
2022-07-17 01:37:05 -0500

    filtro de redes por id

--- Proyecto02/js/network.js
+++ Proyecto02/js/network.js
@@ -1,48 +1,5 @@
-// Graph
-var ctx = document.getElementById("myChart");
-var myChart = new Chart(ctx, {
-  type: "line",
-  data: {
-    labels: [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ],
-    datasets: [
-      {
-        data: [15339, 21345, 18483, 24003, 23489, 24092, 12034],
-        lineTension: 0,
-        backgroundColor: "transparent",
-        borderColor: "#007bff",
-        borderWidth: 4,
-        pointBackgroundColor: "#007bff",
-      },
-    ],
-  },
-  options: {
-    scales: {
-      yAxes: [
-        {
-          ticks: {
-            beginAtZero: false,
-          },
-        },
-      ],
-    },
-    legend: {
-      display: false,
-    },
-  },
-});
-
-const URL= "ttp://api.citybik.es/v2/networks/";
-
-
-var arregloPaises = []
+const URL= "http://api.citybik.es/v2/networks/";
+var mapaStation = new Map();
 async function cargarDatos() {
     const redes = await this.GetCityBikes(); 
     for(let  red of redes) {
@@ -50,31 +7,52 @@ async function cargarDatos() {
         let locacion = red.location;
         let nombre = red.name;
         let id = red.id;
-        let plantilla = 
+        let plantilla =`
+        <option value="${id}">${id}</option>
+    `
+            // se agrega la etiqueta <option> dentro de la etiqueta <select>
+        document.querySelector('select').innerHTML+= plantilla;
+    }
+    const selectElement = document.querySelector('select');
+    selectElement.addEventListener('change', (event)=>{
+        const result = event.target.value;
+        document.getElementById("nombreRed").innerHTML="Red - "+result;
+        console.log(result);
+        fetch(URL+result)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('elemento').innerHTML='';
+            let id = data.network.id;
+            let nombre = data.network.name;
+            let arraystation= data.network.stations;
+            let claves=[];
+            let valores=[];
+            for( const station of arraystation){
+                console.log(station);
+                let plantilla = 
         `
                         <tr>
+                            <td>${id}</td>
                             <td>${nombre}</td>
-                            <td>${compania}</td>
-                            <td>${locacion.city}</td>
+                            <td>${data.network.location.country} - ${data.network.location.city}</td>
+                            <td>${station.name}</td>
+                            <td> ${station.free_bikes}</td>
                         </tr>
-                        `
-            document.getElementById('elemento').innerHTML+= plantilla;
-            if (arregloPaises.includes(locacion.country)!=true){
-                arregloPaises.push(locacion.country)
-            }
-        }
-        for(const item of arregloPaises){
-            let plantilla =`
-                <option value="${item}">${item}</option>
-            `
-            // se agrega la etiqueta <option> dentro de la etiqueta <select>
-            document.querySelector('select').innerHTML+= plantilla;
-        }
-        
-        
-    };
-    
 
+                        `
+        document.getElementById('elemento').innerHTML+= plantilla;
+        mapaStation.set(station.name,station.free_bikes)
+        }
+        mapaStation.forEach (function(value, key) {
+            claves.push(key);
+            valores.push(value);
+        })
+        chart(claves,valores);
+        })
+    });
+    
+    
+}
 
 async function GetCityBikes(){
 return fetch("http://api.citybik.es/v2/networks/")
@@ -89,6 +67,39 @@ return array;
 }); 
 }
 
+function chart(labels,data) {
+    var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+  type: "doughnut",
+  data: {
+    labels: labels,
+    datasets: [
+      {
+        data: data,
+        lineTension: 0,
+        backgroundColor: 'rgb(255, 205, 86)',
+        borderColor: "#007bff",
+        borderWidth: 4,
+        pointBackgroundColor: "#007bff",
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Doughnut Chart'
+      }
+    }
+  },
+});
+labels=[];
+data=[]
+}
 
 window.addEventListener('DOMContentLoaded', (event) => {
 //se llama el callback(una funcion como parametro de otra funcion)
